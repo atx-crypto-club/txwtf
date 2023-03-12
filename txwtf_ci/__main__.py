7dc34938cf3c5c83cf7a0bf60dea1ac8c1e9f105
Joe Rivera <j@jriv.us>
2023-03-12 18:27:55 -0500

    forgot to change txwtf_ci

--- txwtf_ci/__main__.py
+++ txwtf_ci/__main__.py
@@ -7,7 +7,7 @@ import click
 
 def _get_default_environment_root():
     return os.environ.get(
-        "ENV_ROOT", join(expanduser("~"), "python-runtime", "sscore"))
+        "ENV_ROOT", join(expanduser("~"), "python-runtime", "txwtf"))
 
 
 def _get_default_edm_install_prefix():
@@ -35,7 +35,7 @@ def _get_default_edm_bin():
     help="EDM root"
 )
 @click.option(
-    "--edm-env", default="sscore-dev",
+    "--edm-env", default="txwtf-dev",
     help="EDM environment to use"
 )
 @click.option(
@@ -66,7 +66,7 @@ def root(context, edm_root, edm_env, edm_py_version, edm_bin):
 @click.pass_obj
 def bootstrap(obj):
     """
-    create EDM environment for running sscore
+    create EDM environment for running txwtf
     """
     if not os.path.isdir(obj.edm_root):
         os.mkdir(obj.edm_root)
@@ -78,7 +78,7 @@ def bootstrap(obj):
 
 source_dir_option = click.option(
     "--source-dir", default=join(dirname(__file__), ".."),
-    help="Directory where sscore/setup.py lives"
+    help="Directory where txwtf/setup.py lives"
 )
 
 
@@ -87,13 +87,13 @@ source_dir_option = click.option(
 @click.pass_obj
 def install_dev(obj, source_dir):
     """
-    Install sscore in the target environment in dev mode (editable)
+    Install txwtf in the target environment in dev mode (editable)
     """
-    SSCORE_ENV_DEPS = ["pyyaml"]
+    TXWTF_ENV_DEPS = ["pyyaml"]
     cmd_base = [obj.edm_bin, "-r", obj.edm_root]
 
     cmd_base + [   # install runtime deps
-        "install", "-e", obj.edm_env, "-y"] + SSCORE_ENV_DEPS,
+        "install", "-e", obj.edm_env, "-y"] + TXWTF_ENV_DEPS,
 
     # install other requirements via pip
     edm_run_cmd = cmd_base + ["run", "-e", obj.edm_env, "--"]
@@ -109,13 +109,13 @@ def install_dev(obj, source_dir):
 @click.pass_obj
 def install(obj, source_dir):
     """
-    Install sscore in the target environment
+    Install txwtf in the target environment
     """
-    SSCORE_ENV_DEPS = ["pyyaml"]
+    TXWTF_ENV_DEPS = ["pyyaml"]
     cmd_base = [obj.edm_bin, "-r", obj.edm_root]
 
     cmd_base + [   # install runtime deps
-        "install", "-e", obj.edm_env, "-y"] + SSCORE_ENV_DEPS,
+        "install", "-e", obj.edm_env, "-y"] + TXWTF_ENV_DEPS,
 
     # install other requirements via pip
     edm_run_cmd = cmd_base + ["run", "-e", obj.edm_env, "--"]
@@ -139,31 +139,31 @@ def migrate(obj):
     # db revision.
     subprocess.check_call(
         edm_run_cmd + [
-        "flask", "--app", "sscore.webapp", "db", "upgrade"])
+        "flask", "--app", "txwtf.webapp", "db", "upgrade"])
 
 
 @root.command()
 @click.pass_obj
 def test(obj):
     """
-    Run tests in sscore EDM environment
+    Run tests in txwtf EDM environment
     """
     edm_run_cmd = [
         obj.edm_bin, "-r", obj.edm_root, "run", "-e", obj.edm_env, "--"]
     subprocess.check_call(
-        edm_run_cmd + ["sscore", "test"], cwd=dirname(__file__))
+        edm_run_cmd + ["txwtf", "test"], cwd=dirname(__file__))
 
 
 @root.command()
 @click.pass_obj
 def flake8(obj):
     """
-    Run flake8 in sscore EDM environment
+    Run flake8 in txwtf EDM environment
     """
     edm_run_cmd = [
         obj.edm_bin, "-r", obj.edm_root, "run", "-e", obj.edm_env, "--"]
     subprocess.check_call(
-        edm_run_cmd + ["sscore", "flake8"], cwd=join(dirname(__file__), ".."))
+        edm_run_cmd + ["txwtf", "flake8"], cwd=join(dirname(__file__), ".."))
 
 
 @root.command()
@@ -181,22 +181,22 @@ def run(obj, cmd_args):
 
 @root.command()
 @click.option(
-    "--log", envvar="SSCORE_LOG", default="-",
+    "--log", envvar="TXWTF_LOG", default="-",
     help="Log file. Use '-' for stdout.")
 @click.option(
     "--log-level", default="WARNING",
     help="Log output level.")
 @click.argument('cmd_args', nargs=-1)
 @click.pass_obj
-def run_sscore(obj, log, log_level, cmd_args):
+def run_txwtf(obj, log, log_level, cmd_args):
     """
-    Run sscore in EDM environment
+    Run txwtf in EDM environment
     """
     edm_run_cmd = [
         obj.edm_bin, "-r", obj.edm_root, "run", "-e", obj.edm_env, "--"]
     subprocess.check_call(
         edm_run_cmd + [
-            "sscore",
+            "txwtf",
             "--log={}".format(log),
             "--log-level={}".format(log_level)] + list(cmd_args))
 
@@ -205,7 +205,7 @@ def run_sscore(obj, log, log_level, cmd_args):
 @click.pass_obj
 def shell(obj):
     """
-    Conveniently drop into the sscore edm environment.
+    Conveniently drop into the txwtf edm environment.
     """
     edm_run_cmd = [
         obj.edm_bin, "-r", obj.edm_root, "shell", "-e", obj.edm_env]
@@ -213,4 +213,4 @@ def shell(obj):
 
 
 if __name__ == '__main__':
-    root(prog_name="sscore_ci")
+    root(prog_name="txwtf_ci")
