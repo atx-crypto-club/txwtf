cc3e4c9be54c3f540a8b44ed31e7ac299878f1f7
Joe Rivera <j@jriv.us>
2023-03-20 20:22:48 -0500

    adding more columns to user table

--- txwtf/webapp/models.py
+++ txwtf/webapp/models.py
@@ -8,3 +8,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    avatar_url = db.Column(db.String(1000))
+    header_image_url = db.Column(db.String(1000))
+    alternate_email = db.Column(db.String(100))
+    email_verified = db.Column(db.Boolean)
+    alternate_email_verified = db.Column(db.Boolean)
