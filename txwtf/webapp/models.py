f9cee32ef0378684fc63f41183e34464350a99b2
Joe Rivera <j@jriv.us>
2023-03-21 19:52:20 -0500

    adding new fields to User and adding change logging

--- txwtf/webapp/models.py
+++ txwtf/webapp/models.py
@@ -10,9 +10,25 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(1000))
     avatar_url = db.Column(db.String(1000))
     header_image_url = db.Column(db.String(1000))
+    card_image_url = db.Column(db.String(1000))
     alternate_email = db.Column(db.String(100))
     email_verified = db.Column(db.Boolean)
     alternate_email_verified = db.Column(db.Boolean)
     description = db.Column(db.String(10000))
     created_time = db.Column(db.DateTime)
     modified_time = db.Column(db.DateTime)
+
+
+class UserChange(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    change_code = db.Column(db.Integer)
+    change_time = db.Column(db.DateTime)
+    change_desc = db.Column(db.String(256))
+
+
+class SystemLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    event_code = db.Column(db.Integer)
+    event_time = db.Column(db.DateTime)
+    event_desc = db.Column(db.String(256))
