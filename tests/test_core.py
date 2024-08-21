fd2746344139dfb22fdd39d84b6e559d2e3ad839
Joe Rivera <j@jriv.us>
2024-08-21 07:32:49 +0000

    moved everything to txwtf.core

--- tests/test_core.py
+++ tests/test_core.py
@@ -78,19 +78,6 @@ class TestCore(unittest.TestCase):
     def tearDown(self):
         SQLModel.metadata.drop_all(self._engine)
 
-    def test_core_stub(self):
-        """
-        Test that our boilerplate works.
-        """
-        # with
-        pass
-
-        # when
-        val = txwtf.core.stub()
-
-        # then
-        self.assertTrue(val)
-
     def test_valid_identifier(self):
         """
         Test valid_identifier
